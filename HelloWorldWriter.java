7fd5058f0e279d685f7cf570ced855b56f408c13
33wolf <hwangce@gmail.com>
2021-08-21 17:24:24 +0800

    Create HelloWorldWriter.java

---
+++ HelloWorldWriter.java
@@ -0,0 +1,124 @@
+
+/*
+ * Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+package io.pravega.example.gettingstarted;
+
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+
+import io.pravega.client.ClientConfig;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import io.pravega.client.EventStreamClientFactory;
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.stream.EventStreamWriter;
+import io.pravega.client.stream.EventWriterConfig;
+import io.pravega.client.stream.ScalingPolicy;
+import io.pravega.client.stream.StreamConfiguration;
+import io.pravega.client.stream.impl.JavaSerializer;
+import java.util.Timer;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import io.pravega.client.stream.*;
+
+/**
+ * A simple example app that uses a Pravega Writer to write to a given scope and stream.
+ */
+public class HelloWorldWriter {
+
+    public final String scope;
+    public final String streamName;
+    public final URI controllerURI;
+
+    public HelloWorldWriter(String scope, String streamName, URI controllerURI) {
+        this.scope = scope;
+        this.streamName = streamName;
+        this.controllerURI = controllerURI;
+    }
+
+    public void run(String routingKey, String message) {
+        StreamManager streamManager = StreamManager.create(controllerURI);
+        final boolean scopeIsNew = streamManager.createScope(scope);
+
+        StreamConfiguration streamConfig = StreamConfiguration.builder()
+                .scalingPolicy(ScalingPolicy.fixed(1))
+                .build();
+        final boolean streamIsNew = streamManager.createStream(scope, streamName, streamConfig);
+
+        try (EventStreamClientFactory clientFactory = EventStreamClientFactory.withScope(scope,
+                ClientConfig.builder().controllerURI(controllerURI).build());
+             EventStreamWriter<String> writer = clientFactory.createEventWriter(streamName,
+                                                                                 new JavaSerializer<String>(),
+                                                                                 EventWriterConfig.builder().build())) {
+
+            System.out.format("Writing message: '%s' with routing-key: '%s' to stream '%s / %s'%n",
+                    message, routingKey, scope, streamName);
+            final CompletableFuture writeFuture = writer.writeEvent(routingKey, message);
+        }
+    }
+
+    public static void main(String[] args) {
+        Options options = getOptions();
+        CommandLine cmd = null;
+        try {
+            cmd = parseCommandLineArgs(options, args);
+        } catch (ParseException e) {
+            System.out.format("%s.%n", e.getMessage());
+            final HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("HelloWorldWriter", options);
+            System.exit(1);
+        }
+
+        final String scope = cmd.getOptionValue("scope") == null ? Constants.DEFAULT_SCOPE : cmd.getOptionValue("scope");
+        final String streamName = cmd.getOptionValue("name") == null ? Constants.DEFAULT_STREAM_NAME : cmd.getOptionValue("name");
+        final String uriString = cmd.getOptionValue("uri") == null ? Constants.DEFAULT_CONTROLLER_URI : cmd.getOptionValue("uri");
+        final URI controllerURI = URI.create(uriString);
+
+        HelloWorldWriter hww = new HelloWorldWriter(scope, streamName, controllerURI);
+
+        final String routingKey = cmd.getOptionValue("routingKey") == null ? Constants.DEFAULT_ROUTING_KEY : cmd.getOptionValue("routingKey");
+        String message = cmd.getOptionValue("message") == null ? Constants.DEFAULT_MESSAGE : cmd.getOptionValue("message");
+        int i1=0;
+        while(true){
+                message = ""+i1;
+                i1 = 1-i1;
+                hww.run(routingKey, message);
+                try {
+                   Thread.sleep(1000);
+                   System.out.print("   线程睡眠1秒！\n");
+                } catch (InterruptedException e) {
+                       e.printStackTrace();
+                }
+        }
+    }
+
+    private static Options getOptions() {
+        final Options options = new Options();
+        options.addOption("s", "scope", true, "The scope name of the stream to read from.");
+        options.addOption("n", "name", true, "The name of the stream to read from.");
+        options.addOption("u", "uri", true, "The URI to the controller in the form tcp://host:port");
+        options.addOption("r", "routingKey", true, "The routing key of the message to write.");
+        options.addOption("m", "message", true, "The message to write.");
+        return options;
+    }
+
+    private static CommandLine parseCommandLineArgs(Options options, String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        return cmd;
+    }
+}
